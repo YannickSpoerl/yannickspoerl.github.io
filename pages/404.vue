5e557d503fb656606ef9bc4a2df9232b509c78fe
Yannick Spoerl <spoerlyannick@gmail.com>
2020-09-25 14:02:50 +0200

    Try no get error page working correctly

--- pages/404.vue
+++ pages/404.vue
@@ -1,111 +1,34 @@
 <template>
-  <div class="content-wrapper-main">
-    <vue-scroll-snap>
-      <div id="hello" class="item">
-        <Hello/>
+  <div class="content-wrapper">
+      <div class="content__default" style="text-align: center">
+        <h1>{{ $t('error.title') }}</h1>
+        <br/>
+        <a class="button nav-link" :href="localePath('/')">
+          {{ $t('error.button') }}
+        </a>
       </div>
-      <div id="me" class="item light">
-        <Me/>
-      </div>
-      <div id="socials" class="item">
-        <Socials/>
-      </div>
-      <div id="blog" class="item light">
-        <Blog/>
-      </div>
-      <div id="apps" class="item">
-        <Apps/>
-      </div>
-      <div id="resume" class="item light">
-        <Resume/>
-      </div>
-      <div id="end" class="item">
-        <End/>
-      </div>
-    </vue-scroll-snap>
   </div>
 </template>
 
 <script>
-import VueScrollSnap from 'vue-scroll-snap'
-
 export default {
-  name: 'Main',
-  components: {
-    VueScrollSnap
-  }
+  props: ['error'],
+  layout: 'default'
 }
 </script>
-
 <style lang="stylus">
 @import '~assets/style/index'
-
-.content-wrapper-main
-  padding-top: 80px
-  width: 100%
-  height: 100%
-  margin 0
+.content-wrapper
+  padding 160px 15px 80px 15px
+  min-height calc(100vh - 80px - 60px - 160px)
+  max-width $contentWidth
+  margin 0 auto
 
   @media (max-width: $MQMobile)
     &
-      padding 65px 0 0 0
-      min-height calc(100vh - 20px - 60px - 60px)
+      padding 100px 15px 20px 15px
+      min-height calc(100vh - 20px - 60px - 100px)
 
-p.text
-  font-size 22px
-  @media (max-width: $MQMobile)
-    &
-      font-size 14px
-
-h1.text
-  font-size 41px
-  @media (max-width: $MQMobile)
-    &
-      font-size 28px
-
-h3.text
-  font-size 28px
-  @media (max-width: $MQMobile)
-    &
-      font-size 20px
-
-.item
-  min-height calc(100vh - 80px - 62px)
-  @media (max-width: $MQMobile)
-    &
-      padding 65px 0 0px 0
-      min-height calc(100vh - 110px - 63px)
-
-.scroll-snap-container
-  width 100%
-  height calc(100vh - 80px - 62px)
-  @media (max-width: $MQMobile)
-    &
-      padding 65px 0 0px 0
-      height calc(100vh - 110px - 63px)
-
-.dark
-  background-color #435466
-  color #ffffff
-
-
-.light
-  background-color: #4dba87
-  color #ffffff
-
-.panel
-  display flex
-  justify-content center
-  flex-direction column
-  text-align center
-  height calc(100vh - 80px - 62px)
-  @media (max-width: $MQMobile)
-    &
-      height calc(100vh - 110px - 63px)
-
-.nav-link
-  text-decoration none
-    
 .button
     display inline-block
     font-size 1.2rem
